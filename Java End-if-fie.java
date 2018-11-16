e1729b3756329a1b86492f7364ab03cdbdbe4a5a
Ankit Yadav <ankravkk@gmail.com>
2018-11-16 23:23:53 +0530

    solutions

---
+++ Java End-if-fie.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scan=new Scanner(System.in);
+        
+        for(int i=1;scan.hasNext();i++)
+
+        {
+            String line=scan.nextLine();
+            System.out.println(i+" "+line);
+        }
+    }
+};
