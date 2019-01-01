8a02e7e426f4750c116cdc0def3f9590bc5e75ab
Ankit Yadav <ankravkk@gmail.com>
2019-01-02 02:25:29 +0530

    Solutions

--- Java End-if-fie.java
+++ Java End-if-fie.java
@@ -4,16 +4,15 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class olution {
 
     public static void main(String[] args) {
        Scanner scan=new Scanner(System.in);
         
         for(int i=1;scan.hasNext();i++)
-
         {
-            String line=scan.nextLine();
-            System.out.println(i+" "+line);
+          String line=scan.nextLine();
+          System.out.println(i+" "+line);
         }
     }
-};
+}
